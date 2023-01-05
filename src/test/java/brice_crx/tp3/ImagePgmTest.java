f84d6c4822c3a3aa81de081f3e11861147687aed
Bricecrx <94597541+Bricecrx@users.noreply.github.com>
2023-01-05 09:48:09 +0100

    not set up tests have been put to commentaries

--- src/test/java/brice_crx/tp3/ImagePgmTest.java
+++ src/test/java/brice_crx/tp3/ImagePgmTest.java
@@ -106,86 +106,87 @@ public class ImagePgmTest {
         instance.setHighestGreyLevel(highestGreyLevel);
     }
 
-    /**
-     * Test of getImage method, of class ImagePgm.
-     */
-    @Test
-    public void testGetImage() {
-        System.out.println("getImage");
-        ImagePgm instance = new ImagePgm(150,200);
-        List<List<Integer>> expResult = new ImagePgm(150,200).getImage();
-        List<List<Integer>> result = instance.getImage();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
-    //TODO: Finir tests à partir d'ici
-
-    /**
-     * Test of seuil method, of class ImagePgm.
-     */
-    @Test
-    public void testSeuil() {
-        System.out.println("seuil");
-        int seuilValue = 0;
-        ImagePgm instance = null;
-        instance.seuil(seuilValue);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of difference method, of class ImagePgm.
-     */
-    @Test
-    public void testDifference() {
-        System.out.println("difference");
-        ImagePgm img = null;
-        ImagePgm instance = null;
-        instance.difference(img);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of agrandissement method, of class ImagePgm.
-     */
-    @Test
-    public void testAgrandissement() {
-        System.out.println("agrandissement");
-        int multiplicateur = 0;
-        ImagePgm instance = null;
-        instance.agrandissement(multiplicateur);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of retrecissement method, of class ImagePgm.
-     */
-    @Test
-    public void testRetrecissement() {
-        System.out.println("retrecissement");
-        int diviseur = 0;
-        ImagePgm instance = null;
-        instance.retrecissement(diviseur);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setPgmExtension method, of class ImagePgm.
-     */
-    @Test
-    public void testSetPgmExtension() {
-        System.out.println("setPgmExtension");
-        String url = "";
-        String expResult = "";
-        String result = ImagePgm.setPgmExtension(url);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//TODO: Préparer ces tests qui ne sont pas encore mis en place
+//    /**
+//     * Test of getImage method, of class ImagePgm.
+//     */
+//    @Test
+//    public void testGetImage() {
+//        System.out.println("getImage");
+//        ImagePgm instance = new ImagePgm(150,200);
+//        List<List<Integer>> expResult = new ImagePgm(150,200).getImage();
+//        List<List<Integer>> result = instance.getImage();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
+//    //TODO: Finir tests à partir d'ici
+//
+//    /**
+//     * Test of seuil method, of class ImagePgm.
+//     */
+//    @Test
+//    public void testSeuil() {
+//        System.out.println("seuil");
+//        int seuilValue = 0;
+//        ImagePgm instance = null;
+//        instance.seuil(seuilValue);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of difference method, of class ImagePgm.
+//     */
+//    @Test
+//    public void testDifference() {
+//        System.out.println("difference");
+//        ImagePgm img = null;
+//        ImagePgm instance = null;
+//        instance.difference(img);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of agrandissement method, of class ImagePgm.
+//     */
+//    @Test
+//    public void testAgrandissement() {
+//        System.out.println("agrandissement");
+//        int multiplicateur = 0;
+//        ImagePgm instance = null;
+//        instance.agrandissement(multiplicateur);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of retrecissement method, of class ImagePgm.
+//     */
+//    @Test
+//    public void testRetrecissement() {
+//        System.out.println("retrecissement");
+//        int diviseur = 0;
+//        ImagePgm instance = null;
+//        instance.retrecissement(diviseur);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setPgmExtension method, of class ImagePgm.
+//     */
+//    @Test
+//    public void testSetPgmExtension() {
+//        System.out.println("setPgmExtension");
+//        String url = "";
+//        String expResult = "";
+//        String result = ImagePgm.setPgmExtension(url);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     
 }
